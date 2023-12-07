a49095201fe3f99699b77426c2cb6abc06d3b914
benjaminespinoza <benjamintwo2002@gmail.com>
2023-12-06 21:41:10 -0300

    lola

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from grafo import Graph ; from dfs import dfs ; from bfs import bfs; from kruskal import kruskal 
-from graficar import visualizar_grafo ;
+from grafo import Graph
+from dfs import dfs
+from bfs import bfs
+from kruskal import kruskal
+from graficar import visualizar_grafo
+from lol import floyd_warshall, minimo, dijkstra
 
 # Crear una instancia de la clase Graph
 grafo = Graph()
@@ -12,8 +16,15 @@ grafo.add_V('D')
 grafo.add_V('E')
 grafo.add_E('A', 'B', 1)
 grafo.add_E('B', 'C', 2)
-grafo.add_E('A' , 'D' , 1)
-grafo.add_E('B' , 'E' , 1)
+grafo.add_E('A', 'D', 1)
+grafo.add_E('B', 'E', 1)
+
+
+Graph.floyd_warshall = floyd_warshall
+print(grafo.floyd_warshall())
+Graph.minimo = minimo
+Graph.dijkstra = dijkstra
+print(grafo.dijkstra('A'))
 
 # Algoritmo DFS
 # print("DFS:")
@@ -25,4 +36,3 @@ grafo.add_E('B' , 'E' , 1)
 
 # gr = kruskal(grafo)
 # visualizar_grafo(grafo , gr , titulo="Grafo Kruskal")
-
